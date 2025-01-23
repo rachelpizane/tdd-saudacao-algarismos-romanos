46c368cd5b3fe853d71292028e45b0993f2285b5
Rachel Pizane <rachel.pzm@gmail.com>
2025-01-23 20:48:57 -0300

    requisitos 3,4,5,6 e 7 concluidos

--- src/main/java/com/startdbmob2/demo/Saudacao.java
+++ src/main/java/com/startdbmob2/demo/Saudacao.java
@@ -1,5 +1,8 @@
 package com.startdbmob2.demo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +13,13 @@ public class Saudacao {
         if(nome == null) {
             return mensagemPadrao;
         }
-        
+
         if(nome.isEmpty()){
             return mensagemPadrao;
+        }
+
+        if (nome.equals(nome.toUpperCase())){
+            return String.format("OLÁ, %s!!!", nome);
         }
 
         return String.format("Olá, %s", nome);
@@ -21,5 +28,67 @@ public class Saudacao {
     public String saudarSimples(){
         return "Olá, você aí";
     }
-    
+
+    public String saudarSimples(List<String> nomes){
+        List<String> nomesFormatados = new ArrayList<>();
+
+        int ultimoIndex;
+        
+        String mensagem = "Olá";
+        String mensagemGritada = " E OLÁ";
+        boolean temNomeGritado = false;
+
+        
+
+        for(int i = 0; i < nomes.size(); i++) {
+            String item = nomes.get(i);
+
+            if(item.contains(",")){
+                String[] itens = item.split((","));
+
+                for(int j = 0; j < itens.length; j++) {
+                    itens[j] = itens[j].trim();
+                    nomesFormatados.add(itens[j]);
+                }
+                continue;
+            }
+            nomesFormatados.add(item);
+
+        }
+
+        ultimoIndex = nomesFormatados.size() - 1;
+
+        if(nomesFormatados.size() == 1){
+            return this.saudarSimples(nomesFormatados.get(0));
+        }
+
+        for(int i = 0; i < nomesFormatados.size(); i++) {
+            String nome = nomesFormatados.get(i);
+            String conexaoTexto = ", ";
+
+            String complementoMensagem;
+
+            if(ultimoIndex == i){
+                conexaoTexto = " e ";
+            }
+
+            complementoMensagem = conexaoTexto + nome;
+
+            if(nome.equals(nome.toUpperCase())){
+                temNomeGritado = true;
+                mensagemGritada = mensagemGritada + complementoMensagem;
+                continue;
+            }
+
+            mensagem  = mensagem + complementoMensagem;
+            System.out.print(mensagem);
+        }
+
+        if(!temNomeGritado){
+            return mensagem;
+        }
+
+        return String.format("%s.%s!!!", mensagem, mensagemGritada);
+    }
+   
 }
--- src/test/java/com/startdbmob2/demo/SaudacaoTests.java
+++ src/test/java/com/startdbmob2/demo/SaudacaoTests.java
@@ -5,6 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -15,9 +18,10 @@ public class SaudacaoTests {
     private Saudacao saudacao;
 
     // Requisito 1
-    // Interpolar o nome com uma saudação simples. Por exemplo, quando o nome é "Maria", o método retorna "Olá, Maria".
+    // Interpolar o nome com uma saudação simples. Por exemplo, quando o nome é
+    // "Maria", o método retorna "Olá, Maria".
     @Test
-    public void saudacaoSimplesTest(){
+    public void saudacaoSimplesTest() {
         String resultado = saudacao.saudarSimples("Maria");
         String esperado = "Olá, Maria";
 
@@ -28,18 +32,108 @@ public class SaudacaoTests {
     // Caso o nome não esteja preenchido, retornar "Olá, você aí".
     @ParameterizedTest
     @NullAndEmptySource
-    public void saudacaoSemPrencher(String nome){
+    public void saudacaoSemPrencher(String nome) {
         String resultado = saudacao.saudarSimples(nome);
         String esperado = "Olá, você aí";
-        
+
         assertEquals(esperado, resultado);
     }
 
     @Test
-    public void saudacaoSemPrencherParametro(){
+    public void saudacaoSemPrencherParametro() {
         String resultado = saudacao.saudarSimples();
         String esperado = "Olá, você aí";
-        
+
         assertEquals(esperado, resultado);
     }
+
+    // Requisito 3
+    // Se o nome for gritado (em maiúsculas), retornar gritando também "OLÁ,
+    // MARIA!!!".
+    @Test
+    public void saudacaoGritada() {
+        String resultado = saudacao.saudarSimples("MARIA");
+        String esperado = "OLÁ, MARIA!!!";
+
+        assertEquals(esperado, resultado);
+    }
+
+    // Requisito 4
+    // Para entrada com dois nomes, retornar os nomes separados com "e". Por exemplo
+    // para ["Maria, "Nina"], retornar "Olá, Maria e Nina".
+    @Test
+    public void saudacaoComListaDeNomesComDoisNomes() {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Maria");
+        nomes.add("Nina");
+
+        String resultado = saudacao.saudarSimples(nomes);
+        String esperado = "Olá, Maria e Nina";
+
+        assertEquals(esperado, resultado);
+    }
+
+    // Requisito 5
+    // Para entrada com mais de dois nomes, separar os nomes com vírgula e o último
+    // com "e".
+    @Test
+    public void saudacaoComListaDeNomesComMaisDeDoisNomes() {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Maria");
+        nomes.add("Nina");
+        nomes.add("Carla");
+
+        String resultado = saudacao.saudarSimples(nomes);
+        String esperado = "Olá, Maria, Nina e Carla";
+
+        assertEquals(esperado, resultado);
+    }
+
+    @Test
+    public void saudacaoComListaDeNomesComUmNome() {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Maria");
+
+        String resultado = saudacao.saudarSimples(nomes);
+        String esperado = "Olá, Maria";
+
+        assertEquals(esperado, resultado);
+    }
+
+    // Requisito 6
+    // Separar nomes gritados em outra saudação. Por exemplo para ["Maria", "LÚCIA",
+    // "Nina"] retornar "Olá Maria e Nina. E OLÁ, LÚCIA!!!"
+    @Test
+    public void saudacaoComListaDeNomesComNomesGritados() {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Maria");
+        nomes.add("LÚCIA");
+        nomes.add("Nina");
+
+        String resultado = saudacao.saudarSimples(nomes);
+        String esperado = "Olá, Maria e Nina. E OLÁ, LÚCIA!!!";
+
+        assertEquals(esperado, resultado);
+    }
+
+    // Requisito 7
+    // Se algum dos nomes incluir vírgula, separe e trate como nomes separados. Por
+    // exemplo para ["Maria, Lúcia", "Nina"], retornar "Olá, Maria, Lúcia e Nina".
+    @Test
+    public void saudacaoComListaDeNomesComNomesComVirgulas() {
+        List<String> nomes = new ArrayList<>();
+
+        nomes.add("Maria, Lúcia");
+        nomes.add("Nina, Carla");
+
+        String resultado = saudacao.saudarSimples(nomes);
+        String esperado = "Olá, Maria, Lúcia, Nina e Carla";
+
+        assertEquals(esperado, resultado);
+    }
+
 }

2a5b12b0936ac6553edd2e727e30371aee331f58
Rachel Pizane <rachel.pzm@gmail.com>
2025-01-26 17:53:38 -0300

    fix: retirei um import desnecessario

--- src/test/java/com/startdbmob2/demo/ConversorNumericoTests.java
+++ src/test/java/com/startdbmob2/demo/ConversorNumericoTests.java
@@ -1,6 +1,5 @@
 package com.startdbmob2.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.params.ParameterizedTest;

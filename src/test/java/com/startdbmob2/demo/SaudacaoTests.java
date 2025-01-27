f38a576bc98d563aa78bb3bcba5676e1b21170d9
Rachel Pizane <rachel.pzm@gmail.com>
2025-01-26 21:24:04 -0300

    requisito 7 e 8 refatorados

--- src/test/java/com/startdbmob2/demo/SaudacaoTests.java
+++ src/test/java/com/startdbmob2/demo/SaudacaoTests.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -14,16 +15,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SaudacaoTests {
+    private List<String> nomes;
+
     @Autowired
     private Saudacao saudacao;
+
+    @BeforeEach
+    public void setUp(){
+        nomes = new ArrayList<String>();
+    }
+
     // Exercício 1 - Saudações
 
     // Usando TDD, escreva uma função pura para cumprimentar o usuário, evoluindo o código com os requisitos na ordem abaixo.
+
     // (Uma função pura é aquela para qual o resultado é sempre o mesmo dada a mesma entrada e não tem efeitos colaterais; ou seja não utiliza informações que não receba por parâmetro, como variáveis globais, e não altera nenhum dado, inclusive os parâmetros). 
 
+
     // Requisito 1
-    // Interpolar o nome com uma saudação simples. Por exemplo, quando o nome é
-    // "Maria", o método retorna "Olá, Maria".
+    // Interpolar o nome com uma saudação simples. Por exemplo, quando o nome é "Maria", o método retorna "Olá, Maria".
     @Test
     public void saudarComUmNome() {
         String resultado = saudacao.saudar("Maria");
@@ -62,12 +72,9 @@ public class SaudacaoTests {
     }
 
     // Requisito 4
-    // Para entrada com dois nomes, retornar os nomes separados com "e". Por exemplo
-    // para ["Maria, "Nina"], retornar "Olá, Maria e Nina".
+    // Para entrada com dois nomes, retornar os nomes separados com "e". Por exemplo para ["Maria, "Nina"], retornar "Olá, Maria e Nina".
     @Test
     public void saudarComListaDeNomesComDoisNomes() {
-        List<String> nomes = new ArrayList<String>();
-
         nomes.add("Maria");
         nomes.add("Nina");
 
@@ -79,8 +86,6 @@ public class SaudacaoTests {
 
     @Test
     public void saudarComListaDeNomesComUmNome() {
-        List<String> nomes = new ArrayList<String>();
-
         nomes.add("Maria");
 
         String resultado = saudacao.saudar(nomes);
@@ -91,8 +96,6 @@ public class SaudacaoTests {
 
     @Test
     public void saudarComListaDeNomesVazia() {
-        List<String> nomes = new ArrayList<String>();
-
         String resultado = saudacao.saudar(nomes);
         String esperado = "Olá, você aí";
 
@@ -100,12 +103,9 @@ public class SaudacaoTests {
     }
 
     // Requisito 5
-    // Para entrada com mais de dois nomes, separar os nomes com vírgula e o último
-    // com "e".
+    // Para entrada com mais de dois nomes, separar os nomes com vírgula e o último com "e".
     @Test
     public void saudarComListaDeNomesComMaisDeDoisNomes() {
-        List<String> nomes = new ArrayList<String>();
-
         nomes.add("Maria");
         nomes.add("Nina");
         nomes.add("Carla");
@@ -118,8 +118,6 @@ public class SaudacaoTests {
 
     @Test
     public void saudacaoComListaDeNomesComUmNome() {
-        List<String> nomes = new ArrayList<String>();
-
         nomes.add("Maria");
 
         String resultado = saudacao.saudar(nomes);
@@ -129,8 +127,7 @@ public class SaudacaoTests {
     }
 
     // Requisito 6
-    // Separar nomes gritados em outra saudação. Por exemplo para ["Maria", "LÚCIA",
-    // "Nina"] retornar "Olá Maria e Nina. E OLÁ, LÚCIA!!!"
+    // Separar nomes gritados em outra saudação. Por exemplo para ["Maria", "LÚCIA", "Nina"] retornar "Olá Maria e Nina. E OLÁ, LÚCIA!!!"
     @ParameterizedTest
     @CsvSource({
             "Maria, LÚCIA, Nina, CARLA, Rachel, VITORIA,'Olá, Maria, Nina e Rachel. E OLÁ, LÚCIA, CARLA E VITORIA!!!'",
@@ -140,8 +137,6 @@ public class SaudacaoTests {
             "Maria, Lúcia, Nina, CARLA, Rachel, Vitoria, 'Olá, Maria, Lúcia, Nina, Rachel e Vitoria. E OLÁ, CARLA!!!'"
     })
     public void saudarComListaDeNomesComNomesGritados(String nome1, String nome2,String nome3, String nome4, String nome5, String nome6, String esperado) {
-        List<String> nomes = new ArrayList<String>();
-
         nomes.add(nome1);
         nomes.add(nome2);
         nomes.add(nome3);
@@ -154,12 +149,9 @@ public class SaudacaoTests {
     }
     
     // Requisito 7
-    // Se algum dos nomes incluir vírgula, separe e trate como nomes separados. Por
-    // exemplo para ["Maria, Lúcia", "Nina"], retornar "Olá, Maria, Lúcia e Nina".
+    // Se algum dos nomes incluir vírgula, separe e trate como nomes separados. Por exemplo para ["Maria, Lúcia", "Nina"], retornar "Olá, Maria, Lúcia e Nina".
     @Test
-    public void saudacaoComListaDeNomesComNomesComVirgulas() {
-        List<String> nomes = new ArrayList<String>();
-
+    public void saudarComListaDeNomesComNomesComVirgulas() {
         nomes.add("Maria, Lúcia");
         nomes.add("Nina, Carla");
 
@@ -172,9 +164,7 @@ public class SaudacaoTests {
     // Requisito 8
     // Permita que a entrada informe vírgulas intencionalmente usando caracteres de escape. Isso pode ser feito da mesma forma que em arquivos CSV, com aspas duplas em volta da entrada. Por exemplo para ["Maria", "\"Nina, Lúcia\""], retornar "Olá, Maria e Nina, Lúcia".
     @Test
-    public void saudacaoComListaDeNomesComEscapes() {
-        List<String> nomes = new ArrayList<String>();
-
+    public void saudarComListaDeNomesComEscapes() {
         nomes.add("Maria");
         nomes.add("Carla, Thaína");
         nomes.add("\"Nina, Lúcia\"");
